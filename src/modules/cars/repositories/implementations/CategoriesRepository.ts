97f666989ba87ba72b05553fd95423c811752cb3
Igor Popadiuk <igordiuk@gmail.com>
2021-05-22 15:37:54 -0300

    Continuando estudos NodeJS - Ignite - Chapter II

--- src/modules/cars/repositories/implementations/CategoriesRepository.ts
+++ src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -1,11 +1,20 @@
-import { Category } from '../model/Category'
-import { ICategoryRepository, ICreateCategoryDTO } from './ICategoriesRepository';
+import { Category } from '../../model/Category'
+import { ICategoryRepository, ICreateCategoryDTO } from '../ICategoriesRepository';
 
 class CategoriesRepository implements ICategoryRepository {
     private categories: Category[];
+    
+    private static INSTANCE: CategoriesRepository;
 
-    constructor() {
+    private constructor() {
         this.categories = [];
+    }
+
+    public static getInstance(): CategoriesRepository {
+        if (!CategoriesRepository.INSTANCE) {
+            CategoriesRepository.INSTANCE = new CategoriesRepository();
+        }
+        return CategoriesRepository.INSTANCE;
     }
 
     create({ name, description}: ICreateCategoryDTO): void {
