1f483ec4fa576927f6fc1d5d39fda78753e00985
Igor Popadiuk <igordiuk@gmail.com>
2021-06-12 17:12:36 -0300

    Ignite - Trilha NodeJS - Chapter III

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,14 +1,19 @@
 import fs from 'fs'
 import csvParse from 'csv-parse'
 import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
+import { inject, injectable } from 'tsyringe';
 
 interface IImportCategory {
     name: string;
     description: string;
 }
 
+@injectable()
 class ImportCategoryUseCase {
-    constructor(private categoriesRepository: ICategoriesRepository) {}
+    constructor(
+        @inject("CategoriesRepository")
+        private categoriesRepository: ICategoriesRepository
+    ) {}
 
     loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
         return new Promise((resolve, reject) => {
@@ -27,6 +32,7 @@ class ImportCategoryUseCase {
                 });
             })
             .on('end', () => {
+                fs.promises.unlink(file.path);
                 resolve(categories);
             })
             .on('error', (err) => {
@@ -42,10 +48,10 @@ class ImportCategoryUseCase {
         categories.map(async (category) => {
             const { name, description } = category;
 
-            const existCategory = this.categoriesRepository.findByName(name);
+            const existCategory = await this.categoriesRepository.findByName(name);
 
             if (!existCategory) {
-                this.categoriesRepository.create({
+                await this.categoriesRepository.create({
                     name,
                     description
                 });
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import 'reflect-metadata';
+
 import { router } from './routes';
+import swaggerFile from './swagger.json';
+
+import './database';
+
+import './shared/container';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use(router);
 
